f946332837832a990d0041d80405f7955f6bd407
Matt Blackman <mateoblackman@gmail.com>
2025-06-07 14:34:16 -0600

    Remove old render GUI

--- src/Systems/RenderDebugGUISystem.h
+++ src/Systems/RenderDebugGUISystem.h
@@ -2,12 +2,6 @@
 
 #include <SDL3/SDL.h>
 
-#include "../Components/BoxColliderComponent.h"
-#include "../Components/HealthComponent.h"
-#include "../Components/ProjectileEmitterComponent.h"
-#include "../Components/RigidBodyComponent.h"
-#include "../Components/SpriteComponent.h"
-#include "../Components/TransformComponent.h"
 #include "../ECS/ECS.h"
 #include "Components/ScriptComponent.h"
 #include "imgui.h"
@@ -20,7 +14,7 @@ class RenderDebugGUISystem : public System {
 
   ~RenderDebugGUISystem() = default;
 
-  void Update(SDL_Renderer* renderer) {
+  void Update(SDL_Renderer* renderer) const {
     ImGui_ImplSDLRenderer3_NewFrame();
     ImGui_ImplSDL3_NewFrame();
     ImGui::NewFrame();
@@ -35,78 +29,7 @@ class RenderDebugGUISystem : public System {
         }
       }
     }
-    Logger::Log("Rendering debug GUI");
     ImGui::Render();
     ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
-  }
-
- private:
-  static void SpawnEnemyWindow(std::unique_ptr<Registry>& registry) {
-    if (ImGui::Begin("Spawn enemy")) {
-      static int xPos = 0, yPos = 0;
-      static float scale = 1.0, rotation = 0.0;
-      static int xVelocity = 0, yVelocity = 0;
-      const char* sprites[] = {"truck-image", "tank-image"};
-      static int spriteSelectedIndex = 0;
-      static float projectileAngle = 0.0, projectileFrequency = 1.0, projectileDuration = 5.0;
-      static int projectileSpeed = 100, projectileDamage = 10;
-      static int maxHealth = 100, startingHealth = 100;
-
-      ImGui::InputInt("Spawn X", &xPos);
-      ImGui::InputInt("Spawn Y", &yPos);
-      ImGui::InputFloat("Scale", &scale);
-      ImGui::InputFloat("Rotation", &rotation);
-
-      ImGui::SeparatorText("Velocity");
-      ImGui::InputInt("X Velocity", &xVelocity);
-      ImGui::InputInt("Y Velocity", &yVelocity);
-
-      ImGui::Separator();
-
-      const char* combo_preview_value = sprites[spriteSelectedIndex];
-      if (ImGui::BeginCombo("Sprite", combo_preview_value)) {
-        for (int n = 0; n < IM_ARRAYSIZE(sprites); n++) {
-          const bool is_selected = (spriteSelectedIndex == n);
-          if (ImGui::Selectable(sprites[n], is_selected)) spriteSelectedIndex = n;
-
-          // Set the initial focus when opening the combo (scrolling + keyboard
-          // navigation focus)
-          if (is_selected) ImGui::SetItemDefaultFocus();
-        }
-        ImGui::EndCombo();
-      }
-
-      ImGui::SeparatorText("Projectile");
-      ImGui::SliderFloat("Projectile Angle", &projectileAngle, 0.0f, 360.0f);
-      ImGui::InputFloat("Projectile Frequency (Seconds)", &projectileFrequency);
-      ImGui::InputFloat("Projectile Duration (Seconds)", &projectileDuration);
-      ImGui::InputInt("Projectile Speed", &projectileSpeed);
-      ImGui::InputInt("Projectile Damage", &projectileDamage);
-
-      ImGui::SeparatorText("Health");
-      ImGui::InputInt("Max Health", &maxHealth);
-      ImGui::InputInt("Starting Health", &startingHealth);
-
-      ImGui::Separator();
-
-      if (ImGui::Button("Spawn")) {
-        auto sprite = std::string(sprites[spriteSelectedIndex]);
-        const auto radians = glm::radians(projectileAngle);
-        auto projectileVelocity = glm::vec2(glm::cos(radians), glm::sin(radians)) * (float)projectileSpeed;
-        auto projectileFrequencyMs = static_cast<int>(projectileFrequency * 1000);
-        auto projectileDurationMs = static_cast<int>(projectileDuration * 1000);
-
-        auto enemy = registry->CreateEntity();
-        enemy.Group("enemies");
-        enemy.AddComponent<TransformComponent>(glm::vec2(xPos, yPos), glm::vec2(scale, scale), rotation);
-        enemy.AddComponent<RigidBodyComponent>(glm::vec2(xVelocity, yVelocity));
-        enemy.AddComponent<SpriteComponent>(sprite, 32, 32, 1);
-        enemy.AddComponent<BoxColliderComponent>(32, 32);
-        enemy.AddComponent<ProjectileEmitterComponent>(projectileVelocity, projectileDurationMs, projectileFrequencyMs,
-                                                       projectileDamage, false);
-        enemy.AddComponent<HealthComponent>(maxHealth, startingHealth);
-      }
-    }
-    ImGui::End();
   }
 };
