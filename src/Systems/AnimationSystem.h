c9f0503793b651fd5c1992a71ddb1b374b02b1e4
Matt Blackman <mateoblackman@gmail.com>
2025-06-09 09:13:12 -0600

    Reduce cognitive complexity

--- src/Systems/AnimationSystem.h
+++ src/Systems/AnimationSystem.h
@@ -26,34 +26,39 @@ class AnimationSystem : public System {
       auto& animation = entity.GetComponent<AnimationComponent>();
       auto& sprite = entity.GetComponent<SpriteComponent>();
 
-      if (animation.numFrames <= 0 || animation.frameRateSpeed <= 0) {
+      if (animation.numFrames <= 0 || animation.frameRateSpeed <= 0 || animation.isFinished) {
         continue;
       }
 
-      animation.frameTimer += deltaTime;
-      const double timePerFrame = 1.0f / static_cast<double>(animation.frameRateSpeed);
-
-      if (animation.frameTimer >= timePerFrame) {
-        const int framesToAdvance = static_cast<int>(std::round(animation.frameTimer / timePerFrame));
-
-        if (animation.shouldLoop) {
-          animation.currentFrame = (animation.currentFrame + framesToAdvance) % animation.numFrames;
-        } else {
-          animation.currentFrame = std::min(animation.currentFrame + framesToAdvance, animation.numFrames - 1);
-          if (animation.currentFrame >= animation.numFrames - 1) {
-            animation.isFinished = true;
-          }
-        }
-
-        if (animation.shouldLoop) {
-          animation.frameTimer = fmod(animation.frameTimer, timePerFrame);
-        } else if (animation.isFinished) {
-          animation.frameTimer = 0.0f;
-        }
-      }
+      UpdateAnimationState(animation, deltaTime);
 
       sprite.srcRect.x = static_cast<float>(animation.currentFrame) * sprite.width;
       // sprite.srcRect.y = animation.currentFrame * sprite.height;
+    }
+  }
+
+ private:
+  void UpdateAnimationState(AnimationComponent& animation, const float deltaTime) const {
+    animation.frameTimer += deltaTime;
+    const double timePerFrame = 1.0f / static_cast<double>(animation.frameRateSpeed);
+
+    if (animation.frameTimer >= timePerFrame) {
+      const int framesToAdvance = static_cast<int>(std::round(animation.frameTimer / timePerFrame));
+
+      if (animation.shouldLoop) {
+        animation.currentFrame = (animation.currentFrame + framesToAdvance) % animation.numFrames;
+      } else {
+        animation.currentFrame = std::min(animation.currentFrame + framesToAdvance, animation.numFrames - 1);
+        if (animation.currentFrame >= animation.numFrames - 1) {
+          animation.isFinished = true;
+        }
+      }
+
+      if (animation.shouldLoop) {
+        animation.frameTimer = fmod(animation.frameTimer, timePerFrame);
+      } else if (animation.isFinished) {
+        animation.frameTimer = 0.0f;
+      }
     }
   }
 };
