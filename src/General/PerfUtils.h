acb0ac57cbafd4a2f6f90064b6d0fb350df6a098
mblackman <mateoblackman@gmail.com>
2025-06-16 09:50:29 -0600

    Added more performance utils to measure time spent in sub-functions

--- src/General/PerfUtils.h
+++ src/General/PerfUtils.h
@@ -33,8 +33,64 @@ class ScopedTimer {
   std::chrono::time_point<std::chrono::high_resolution_clock> m_start;
 };
 
-#define PROFILE_SCOPE PerfUtils::ScopedTimer timer(__FUNCTION__)
-#define PROFILE_NAMED_SCOPE(name) PerfUtils::ScopedTimer timer(name)
+class ProfilingAccumulator {
+ public:
+  static void Add(const std::string &name, const long long duration_microseconds) {
+    std::lock_guard lock(s_mutex);
+    s_accumulated_times[name] += duration_microseconds;
+  }
+
+  static void Reset() {
+    std::lock_guard lock(s_mutex);
+    s_accumulated_times.clear();
+  }
+
+  static void Report() {
+    std::lock_guard lock(s_mutex);
+    Logger::Info("--- Accumulated Performance Report ---");
+    for (const auto &[name, total_duration] : s_accumulated_times) {
+      const auto ms = static_cast<double>(total_duration.load()) * 0.001;
+      Logger::Info("ACCUM: " + name + ": " + std::to_string(ms) + "ms");
+    }
+    Logger::Info("------------------------------------");
+  }
+
+ private:
+  inline static std::mutex s_mutex;
+  inline static std::map<std::string, std::atomic<long long>> s_accumulated_times;
+};
+
+class AccumulatingScopedTimer {
+ public:
+  explicit AccumulatingScopedTimer(std::string name)
+      : m_name(std::move(name)), m_start(std::chrono::high_resolution_clock::now()) {}
+
+  ~AccumulatingScopedTimer() {
+    const auto end = std::chrono::high_resolution_clock::now();
+    const auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - m_start).count();
+    ProfilingAccumulator::Add(m_name, duration);
+  }
+
+  AccumulatingScopedTimer(const AccumulatingScopedTimer &) = delete;
+  AccumulatingScopedTimer &operator=(const AccumulatingScopedTimer &) = delete;
+  AccumulatingScopedTimer(AccumulatingScopedTimer &&) = delete;
+  AccumulatingScopedTimer &operator=(AccumulatingScopedTimer &&) = delete;
+
+ private:
+  std::string m_name;
+  std::chrono::time_point<std::chrono::high_resolution_clock> m_start;
+};
+
+class AggregateProfilingSession {
+ public:
+  AggregateProfilingSession() { ProfilingAccumulator::Reset(); }
+  ~AggregateProfilingSession() { ProfilingAccumulator::Report(); }
+
+  AggregateProfilingSession(const AggregateProfilingSession &) = delete;
+  AggregateProfilingSession &operator=(const AggregateProfilingSession &) = delete;
+  AggregateProfilingSession(AggregateProfilingSession &&) = delete;
+  AggregateProfilingSession &operator=(AggregateProfilingSession &&) = delete;
+};
 
 template <typename Func>
 auto LogTime(const std::string &name, Func &&func) -> std::invoke_result_t<Func> {
@@ -56,3 +112,9 @@ auto LogTime(const std::string &name, Func &&func) -> std::invoke_result_t<Func>
   }
 }
 }  // namespace PerfUtils
+
+#define PROFILE_SCOPE PerfUtils::ScopedTimer timer(__FUNCTION__)
+#define PROFILE_NAMED_SCOPE(name) PerfUtils::ScopedTimer timer(name)
+
+#define ACCUMULATE_PROFILE_SCOPE(name) PerfUtils::AccumulatingScopedTimer accTimer##__LINE__(name)
+#define AGGREGATE_PROFILE_SESSION PerfUtils::AggregateProfilingSession aggSession##__LINE__
