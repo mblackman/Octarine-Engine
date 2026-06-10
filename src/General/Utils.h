a40c977e2ffb0a2482b02cb9eb51233af6ea521b
mblackman <mblackman@users.noreply.github.com>
2026-06-09 22:29:04 -0600

    Show more performance metrics in-game. Average FPS, 95th percentile FPS, and 99th percentile FPS

---
+++ src/General/Utils.h
@@ -0,0 +1,43 @@
+﻿//
+// Created by mateo on 6/9/2026.
+//
+
+#pragma once
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <type_traits>
+
+inline std::vector<std::string> split_string(const std::string& str, const char delimiter)
+{
+    std::vector<std::string> tokens;
+    std::istringstream stream(str);
+    std::string token;
+
+    while (std::getline(stream, token, delimiter))
+    {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+template <typename T>
+bool HasFlag(T value, T flag)
+{
+    using U = std::underlying_type_t<T>;
+    return (static_cast<U>(value) & static_cast<U>(flag)) == static_cast<U>(flag);
+}
+
+inline float get_average(const std::vector<float>& vec)
+{
+    if (vec.empty())
+    {
+        return 0.0f;
+    }
+
+    float sum = std::accumulate(vec.begin(), vec.end(), 0.0f);
+
+    return sum / vec.size();
+}
